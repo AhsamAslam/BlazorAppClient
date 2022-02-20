274020c5ce32f8382ea63e59fc3c76eeabb99d09
Syed Muhammad Ahsam <syed.ahsam@pemmex.com>
2022-02-20 15:14:34 +0500

    main

--- MyCommunityBuilder.Client/MyCommunityBuilder.Identity/Areas/Identity/Pages/Account/RegisterConfirmation.cshtml.cs
+++ MyCommunityBuilder.Client/MyCommunityBuilder.Identity/Areas/Identity/Pages/Account/RegisterConfirmation.cshtml.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
+using System;
+using MyCommunityBuilder.Identity.Helpers;
 
 namespace MyCommunityBuilder.Identity.Areas.Identity.Pages.Account
 {
@@ -30,33 +32,41 @@ namespace MyCommunityBuilder.Identity.Areas.Identity.Pages.Account
 
         public async Task<IActionResult> OnGetAsync(string email, string returnUrl = null)
         {
-            if (email == null)
+            try
             {
-                return RedirectToPage("/Index");
-            }
+                if (email == null)
+                {
+                    return RedirectToPage("/Index");
+                }
 
-            var user = await _userManager.FindByEmailAsync(email);
-            if (user == null)
+                var user = await _userManager.FindByEmailAsync(email);
+                if (user == null)
+                {
+                    return NotFound($"Unable to load user with email '{email}'.");
+                }
+
+                Email = email;
+                // Once you add a real email sender, you should remove this code that lets you confirm the account
+                DisplayConfirmAccountLink = true;
+                if (DisplayConfirmAccountLink)
+                {
+                    var userId = await _userManager.GetUserIdAsync(user);
+                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                    code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+                    EmailConfirmationUrl = Url.Page(
+                        "/Account/ConfirmEmail",
+                        pageHandler: null,
+                        values: new { area = "Identity", userId = userId, code = code, returnUrl = returnUrl },
+                        protocol: Request.Scheme);
+                }
+
+                return Page();
+            }
+            catch(Exception ex)
             {
-                return NotFound($"Unable to load user with email '{email}'.");
+                LogService.WriteLogLine(ex.ToString());
+                throw;
             }
-
-            Email = email;
-            // Once you add a real email sender, you should remove this code that lets you confirm the account
-            DisplayConfirmAccountLink = true;
-            if (DisplayConfirmAccountLink)
-            {
-                var userId = await _userManager.GetUserIdAsync(user);
-                var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-                EmailConfirmationUrl = Url.Page(
-                    "/Account/ConfirmEmail",
-                    pageHandler: null,
-                    values: new { area = "Identity", userId = userId, code = code, returnUrl = returnUrl },
-                    protocol: Request.Scheme);
-            }
-
-            return Page();
         }
     }
 }
