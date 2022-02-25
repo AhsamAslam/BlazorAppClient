51fd2fe6fa4fd221738e0014d8c48dfdfafcf823
Syed Muhammad Ahsam <syed.ahsam@pemmex.com>
2022-02-25 13:10:24 +0500

    main

--- MyCommunityBuilder.Client/MyCommunityBuilder.Identity/Helpers/LogService.cs
+++ MyCommunityBuilder.Client/MyCommunityBuilder.Identity/Helpers/LogService.cs
@@ -10,12 +10,12 @@ namespace MyCommunityBuilder.Identity.Helpers
     {
         public static void WriteLogLine(string strComments)
         {
-            var PathBuild = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\Logs");
+            var PathBuild = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "wwwroot\\Logs");
             if (!Directory.Exists(PathBuild))
             {
                 Directory.CreateDirectory(PathBuild);
             }
-            var fileName = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\Logs\\Logs") + "_" + DateTime.Now.ToString("yyyy") + ".txt";
+            var fileName = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "wwwroot\\Logs\\Logs") + "_" + DateTime.Now.ToString("yyyy") + ".txt";
             using (StreamWriter file = new StreamWriter(fileName, true))
             {
                 file.WriteLine("WriteLogLine Comments: [" + DateTime.Now.ToString() + "] " + strComments + ".");
